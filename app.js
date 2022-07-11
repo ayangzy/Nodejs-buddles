2bba377a8ea38a8efd35af1a07d977368d7c63ef
Ayange Felix <ayangefelix8@gmail.com>
2022-07-11 14:22:00 +0100

    add middleware for error handling

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const productRoutes = require('./routes/productRoute');
 
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 app.use(bodyParser.json());
 
 
 app.use('/api/v1/products', productRoutes);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
